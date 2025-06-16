e0ae61b0c5d023798a6ec68a69a91c562858a967
Ahmed Osama <ahmed418253@feng.bu.edu.eg>
2025-06-16 18:53:18 +0300

    Application User Model (Identity)

---
+++ Domain/Models/Identity/ApplicationUser.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models.Identity
+{
+    public enum UserType
+    {
+        Student = 1,
+        Teacher = 2,
+    }
+
+    public enum Gender
+    {
+        Male = 1,
+        Female = 2,
+    }
+
+    public class ApplicationUser : IdentityUser
+    {
+
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public DateOnly DateOfBirth { get; set; } = default!;
+        public UserType UserType { get; set; } = default!;
+
+    }
+}
