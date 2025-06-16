15ed5a63b8313281cae59b243580d2a5c18611f3
Ahmed Osama <ahmed418253@feng.bu.edu.eg>
2025-06-16 20:25:51 +0300

    Identity Registration & Identity DB Context & Connection String & Migrations

---
+++ Persistence/Identity/TaskHiveIdentityDbContext.cs
@@ -0,0 +1,43 @@
+﻿using Domain.Models.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Identity
+{
+    public class TaskHiveIdentityDbContext(DbContextOptions<TaskHiveIdentityDbContext> options) : IdentityDbContext<ApplicationUser>(options)
+    {
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.Entity<ApplicationUser>().ToTable("Users");
+            builder.Entity<IdentityRole>().ToTable("Roles");
+            builder.Entity<IdentityUserRole<string>>().ToTable("UserRoles");
+
+            builder.Entity<ApplicationUser>().Property(T => T.FirstName)
+                .IsRequired()
+                .HasColumnType("nvarchar(15)");
+
+            builder.Entity<ApplicationUser>().Property(T => T.LastName)
+                .IsRequired()
+                .HasColumnType("nvarchar(15)");
+
+            builder.Entity<ApplicationUser>().Property(T => T.DateOfBirth)
+                .IsRequired()
+                .HasColumnType("date");
+
+            builder.Entity<ApplicationUser>().Property(T => T.UserType)
+                .HasConversion<string>()
+                .IsRequired()
+                .HasColumnType("varchar(7)");
+        }
+
+    }
+}
---
+++ Persistence/InfrastructureServicesRegisteration.cs
@@ -0,0 +1,45 @@
+﻿using Domain.Models.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Persistence.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence
+{
+    public static class InfrastructureServicesRegisteration
+    {
+
+        public static IServiceCollection AddInfrastructureRegistration(this IServiceCollection services, IConfiguration configurations)
+        {
+            services.AddDbContext<TaskHiveIdentityDbContext>(options =>
+                            options.UseSqlServer(configurations.GetConnectionString("IdentityTaskHiveConnection")));
+
+            services.RegisterIdentity();
+
+            return services;
+        }
+
+        private static IServiceCollection RegisterIdentity(this IServiceCollection services)
+        {
+
+            //services.AddIdentity<ApplicationUser, IdentityRole>();
+
+            services.AddIdentityCore<ApplicationUser>(config =>
+            {
+                config.User.RequireUniqueEmail = true;
+            })
+                    .AddRoles<IdentityRole>()
+                    .AddEntityFrameworkStores<TaskHiveIdentityDbContext>();
+
+            return services;
+        }
+
+    }
+}
