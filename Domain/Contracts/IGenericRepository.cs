17796973ec1e461eebf61c764654e9b4e7a4140e
Ahmed Osama <ahmed418253@feng.bu.edu.eg>
2025-06-26 03:38:03 +0300

    Generic Repo & Unit of work Contract

---
+++ Domain/Contracts/IGenericRepository.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Contracts
+{
+    public interface IGenericRepository<TEntity,TKey> where TEntity : BaseEntity<TKey>
+    {
+        public Task<IEnumerable<TEntity>> GetAllAsync();
+        public Task<TEntity> GetByIdAsync(TKey id);
+        public void Add(TEntity entity);
+        public void Update(TEntity entity);
+        public void Delete(TEntity entity);
+    }
+}
