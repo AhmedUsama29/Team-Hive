8ea9bcdb2b07e1b42e5f866463724c083ec72667
Ahmed Osama <ahmed418253@feng.bu.edu.eg>
2025-07-01 14:15:20 +0300

    Service manager with factory delegate

---
+++ ServicesAbstraction/IServiceManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServicesAbstraction
+{
+    public interface IServiceManager
+    {
+
+        public ITaskService TaskService { get; }
+
+    }
+}
